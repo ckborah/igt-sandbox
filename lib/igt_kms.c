69344312d17065800422ee1727122339a8ff84f8
Alex Hung <alex.hung@amd.com>
2024-01-04 13:12:55 -0700

    lib/igt_kms: increase MAX_NUM_COLOROPS to 128
    Signed-off-by: Alex Hung <alex.hung@amd.com>

--- lib/igt_kms.c
+++ lib/igt_kms.c
@@ -90,7 +90,7 @@
 #define MAX_CONNECTORS 32
 #define MAX_EDID 2
 #define DISPLAY_TILE_BLOCK 0x12
-#define MAX_NUM_COLOROPS 32
+#define MAX_NUM_COLOROPS 128
 
 typedef bool (*igt_connector_attr_set)(int dir, const char *attr, const char *value);
 
